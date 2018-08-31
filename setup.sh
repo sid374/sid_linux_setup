5720dc95fbb3450c7aa2fb3b8c659cee6dc1e539
Siddharth Bidasaria <siddharth.bidasaria@rubrik.com>
2018-08-30 19:07:49 -0700

    Add shell script to copy zshrc file

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo $SHELL
+echo "Setting up your workspace"
+read -p "Do you want to install zsh? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    apt-get update
+    apt-get install zsh -y
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+
+read -p "Do you want to setup a zshrc? [y/n]" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    cp zshrc ~/.zshrc
+fi
