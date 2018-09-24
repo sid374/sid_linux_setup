34f8a71bfa149f17343da0c0fdf3e343ae57ee40
Siddharth Bidasaria <siddharth.bidasaria@rubrik.com>
2018-09-24 14:02:20 -0700

    Make agnoster default theme

--- setup.sh
+++ setup.sh
@@ -14,5 +14,7 @@ read -p "Do you want to setup a zshrc? [y/n]" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    cp zshrc ~/.zshrc
+    cp zshrc2 ~/.zshrc
+    cp agnoster.zsh-theme ~/.oh-my-zsh/custom/themes/
+    source ~/.zshrc
 fi
